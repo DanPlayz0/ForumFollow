206268aef7937fc07e43f62f13b895be64f1b96e
DanPlayz0 <damuffin36@gmail.com>
2022-09-25 08:33:14 +0000

    Guild Events

---
+++ events/guildCreate.js
@@ -0,0 +1,19 @@
+const Event = require('@structures/framework/Event');
+module.exports = class extends Event {
+  constructor(client) {
+    super(client, {
+      enabled: true,
+    });
+  }
+
+  async run(client, guild) {
+    console.log(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.ownerId}`);
+    
+    const owner = await guild.fetchOwner();
+    const e = new client.discord.MessageEmbed()
+      .setTitle(`JOINED \`${guild.name}\``)
+      .setColor('#36393E')
+      .setDescription(`Members: ${guild.memberCount}\nID: ${guild.id}\nOwner: ${owner.user.username} (${guild.ownerId})`);
+    client.webhooks.guilds.send({embeds: [e], allowedMentions: { parse: [] }});
+  }
+}
---
+++ events/guildDelete.js
@@ -0,0 +1,20 @@
+const Event = require('@structures/framework/Event');
+module.exports = class extends Event {
+  constructor(client) {
+    super(client, {
+      enabled: true,
+    });
+  }
+
+  async run(client, guild) {
+    if(!guild.available) return;
+    console.log(`[GUILD LEAVE] ${guild.name} (${guild.id}) removed the bot.`);
+
+    const owner = await client.users.fetch(guild.ownerId);
+    const e = new client.discord.MessageEmbed()
+      .setTitle(`LEFT \`${guild.name}\``)
+      .setColor('#36393E')
+      .setDescription(`Members: ${guild.memberCount}\nID: ${guild.id}\nOwner: ${owner.username} (${guild.ownerId})`);
+    client.webhooks.guilds.send({embeds: [e], allowedMentions: { parse: [] }});
+  }
+}
