c601112919c744b1d668b1e0806cecc22812a5fd
DanPlayz0 <damuffin36@gmail.com>
2023-08-18 23:59:57 +0000

    more docker env config updates

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
-let private;
-try {
-  private = require('./config-private');
-} catch {}
 module.exports = {
   // Bot Token
-  token: process.env?.DISCORD_TOKEN ?? private.token,
+  token: process.env.DISCORD_TOKEN,
 
   // Bot Administators (Access to Admin Dash & System Commands)
   admins: ['209796601357533184', '229285505693515776'],
   
   // Database Crap (MongoDB)
-  mongo_uri: process.env?.MONGODB_URI ?? private.mongo_uri,
+  mongo_uri: process.env.MONGODB_URI,
   
   // Support server. (For the "dashboard.example/join")
   supportServerInvite: "https://discord.gg/KkS6yP8",
@@ -22,9 +18,9 @@ module.exports = {
   
   // Bot Logging (Webhooks)
   webhooks: [
-    { name: "shard", url: process.env?.WEBHOOK_SHARD ?? private.webhooks?.shard },
-    { name: "error", url: process.env?.WEBHOOK_ERROR ?? private.webhooks?.error },
-    { name: "command", url: process.env?.WEBHOOK_COMMAND ?? private.webhooks?.command },
-    { name: "guilds", url: process.env?.WEBHOOK_GUILD ?? private.webhooks?.guilds },
+    { name: "shard", url: process.env.WEBHOOK_SHARD },
+    { name: "error", url: process.env.WEBHOOK_ERROR },
+    { name: "command", url: process.env.WEBHOOK_COMMAND },
+    { name: "guilds", url: process.env.WEBHOOK_GUILD },
   ],
 }
