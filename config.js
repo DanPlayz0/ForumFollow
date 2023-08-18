f5f13009bce1ec8b7caed2f729956f0fd4857b82
DanPlayz0 <damuffin36@gmail.com>
2023-08-18 23:38:07 +0000

    env stuff?

--- config.js
+++ config.js
@@ -1,42 +1,30 @@
 let private;
 try {
-  private = {
-    token: process.env.DISCORD_TOKEN,
-    mongo_uri: process.env.MONGODB_URI,
-    webhooks: {
-      shard: process.env.WEBHOOK_SHARD,
-      error: process.env.WEBHOOK_ERROR,
-      command: process.env.WEBHOOK_COMMAND,
-      guilds: process.env.WEBHOOK_GUILD,
-    } 
-  }
-} catch {}
-try {
   private = require('./config-private');
 } catch {}
 module.exports = {
   // Bot Token
-  token: private.token,
+  token: process.env?.DISCORD_TOKEN ?? private.token,
 
   // Bot Administators (Access to Admin Dash & System Commands)
   admins: ['209796601357533184', '229285505693515776'],
   
   // Database Crap (MongoDB)
-  mongo_uri: private.mongo_uri,
+  mongo_uri: process.env?.MONGODB_URI ?? private.mongo_uri,
   
   // Support server. (For the "dashboard.example/join")
   supportServerInvite: "https://discord.gg/KkS6yP8",
  
   // Restful API
   restapi: {
-    port: 3000, 
+    port: 3000,
   },
   
   // Bot Logging (Webhooks)
   webhooks: [
-    { name: "shard", url: private.webhooks?.shard },
-    { name: "error", url: private.webhooks?.error },
-    { name: "command", url: private.webhooks?.command },
-    { name: "guilds", url: private.webhooks?.guilds },
+    { name: "shard", url: process.env?.WEBHOOK_SHARD ?? private.webhooks?.shard },
+    { name: "error", url: process.env?.WEBHOOK_ERROR ?? private.webhooks?.error },
+    { name: "command", url: process.env?.WEBHOOK_COMMAND ?? private.webhooks?.command },
+    { name: "guilds", url: process.env?.WEBHOOK_GUILD ?? private.webhooks?.guilds },
   ],
 }
